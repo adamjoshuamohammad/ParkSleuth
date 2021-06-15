2c4ec93cc25c07241dc3390adac7339911916b8d
Adam <73608454+adamjoshuamohammad@users.noreply.github.com>
2021-06-15 09:28:31 -0400

    Update AppDelegate.swift
    removed api key

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                   $0.server = "https://parseapi.back4app.com/"
         }
         Parse.initialize(with: parseConfig)
-        GMSServices.provideAPIKey("AIzaSyDvFkv2h6M4O1GSkR1xz_b2v5UU21Ps8zo")
+        GMSServices.provideAPIKey("")
         return true
     }
 
